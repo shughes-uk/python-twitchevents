3b6be16d9f5cd947b7917d359ab06fb21d7d6dd7
SHughes <shughes.uk@gmail.com>
2015-12-18 19:47:14 +0000

    dependencies

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup
-setup(name="twitcher", version="0.1", packages=['twitcher'],)
+setup(name="twitcher", install_requires=['python-twitch'],version="0.1", packages=['twitcher'],)
